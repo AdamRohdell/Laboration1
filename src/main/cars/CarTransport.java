a1222abebcd43d2c8df0c2d0e8ff071f2d091e79
AdamRohdell <rohdell@student.chalmers.se>
2018-11-22 13:37:24 +0100

    CarTransport update

--- src/main/cars/CarTransport.java
+++ src/main/cars/CarTransport.java
@@ -1,11 +1,25 @@
 package main.cars;
 
+import main.flatbeds.Ramp;
+
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class CarTransport extends Car {
 
+    private Stack<Car> cars = new Stack<Car>();
+    private Ramp ramp = new Ramp(0, 1);
+
     public CarTransport() {
         super(2, 500, Color.green);
+    }
+
+    @Override
+    public void startEngine(){
+        raiseRamp();
+        super.startEngine();
     }
 
     @Override
@@ -23,5 +37,21 @@ public class CarTransport extends Car {
 
     }
 
+    public void loadCar(Car c){
+        cars.push(c);
+    }
 
+    public Car unloadCar(){
+        return cars.pop();
+    }
+
+    public void lowerRamp(){
+        if (getCurrentSpeed() == 0){
+            ramp.lowerRamp();
+        }
+    }
+
+    public void raiseRamp(){
+        ramp.raiseRamp();
+    }
 }
